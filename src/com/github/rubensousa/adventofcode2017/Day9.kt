59647190934098efbe2f3c448e2f808e26a1a8d4
ruben <rubensousa.mieti@gmail.com>
2017-12-10 19:57:07 +0000

    Add solution and data file for day 9

--- src/com/github/rubensousa/adventofcode2017/Day9.kt
+++ src/com/github/rubensousa/adventofcode2017/Day9.kt
@@ -12,30 +12,30 @@ object Day9 {
     @Throws(IOException::class)
     @JvmStatic
     fun main(args: Array<String>) {
-        val bufferedReader = BufferedReader(FileReader("input.txt"))
+        val bufferedReader = BufferedReader(FileReader("inputday9.txt"))
         var line: String? = bufferedReader.readLine()
         if (line != null) {
             println(getScore(line))
+            println(getGarbage(line))
         }
     }
 
     private fun getScore(line: String): Int {
-        val list = ArrayDeque<Int>()
+        val list = Stack<Int>()
         var garbage = 0
         var current = 0
         var score = 0
         var i = 0
-
         while (i < line.length){
             val c = line[i]
             if (garbage == 0) {
                 when (c) {
                     '{' -> {
-                        list.addFirst(current + 1)
+                        list.push(current + 1)
                         current++
                     }
                     '}' -> {
-                        score += list.removeFirst()
+                        score += list.pop()
                         current--
                     }
                     '<' -> garbage++
@@ -47,28 +47,26 @@ object Day9 {
             }
             i++
         }
-
         return score
     }
 
     private fun getGarbage(line: String): Int {
-        val list = ArrayDeque<Int>()
+        val list = Stack<Int>()
         var garbage = 0
         var current = 0
         var score = 0
         var i = 0
         var garbageCount = 0
-
         while (i < line.length){
             val c = line[i]
             if (garbage == 0) {
                 when (c) {
                     '{' -> {
-                        list.addFirst(current + 1)
+                        list.push(current + 1)
                         current++
                     }
                     '}' -> {
-                        score += list.removeFirst()
+                        score += list.pop()
                         current--
                     }
                     '<' -> garbage++
@@ -82,7 +80,6 @@ object Day9 {
             }
             i++
         }
-
         return garbageCount
     }
 
