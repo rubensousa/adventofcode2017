e44b5f1c5f84595c3f7e33770837cc01e3641eea
ruben <rubensousa.mieti@gmail.com>
2017-12-26 22:44:04 +0000

    Optimize parent search and strength count

--- src/com/github/rubensousa/adventofcode2017/Day24.kt
+++ src/com/github/rubensousa/adventofcode2017/Day24.kt
@@ -5,7 +5,6 @@ import java.io.FileReader
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashSet
-import kotlin.collections.LinkedHashSet
 
 
 object Day24 {
@@ -15,15 +14,28 @@ object Day24 {
 
     data class Result(val length: Int, val strength: Int)
 
-    class ComponentNode(val component: Component) {
+    class Node(val component: Component) {
 
-        var parents = LinkedHashSet<Component>()
+        var parent: Node? = null
+        var length = 1
+        var strength = component.firstPort + component.secondPort
         var firstPortConnected = false
         var secondPortConnected = false
 
-        fun connect(node: ComponentNode): Boolean {
-            if (node.component == component || parents.contains(node.component)) {
+        override fun toString(): String {
+            return component.toString()
+        }
+
+        fun connect(node: Node): Boolean {
+            if (node.component == component) {
                 return false
+            }
+            var currentParent = parent
+            while (currentParent != null) {
+                if (currentParent.component == node.component) {
+                    return false
+                }
+                currentParent = currentParent.parent
             }
             if (!firstPortConnected) {
                 if (component.firstPort == node.component.firstPort && !node.firstPortConnected) {
@@ -45,30 +57,6 @@ object Day24 {
             }
             return false
         }
-
-        override fun hashCode(): Int {
-            return component.hashCode()
-        }
-
-        override fun toString(): String {
-            return component.toString()
-        }
-
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (javaClass != other?.javaClass) return false
-
-            other as ComponentNode
-
-            if (component != other.component) return false
-            if (parents != other.parents) return false
-            if (firstPortConnected != other.firstPortConnected) return false
-            if (secondPortConnected != other.secondPortConnected) return false
-
-            return true
-        }
-
-
     }
 
     fun parseComponents(): ArrayList<Component> {
@@ -98,14 +86,14 @@ object Day24 {
 
     private fun getStrongestBridge(components: List<Component>): Int {
         var strength = 0
-        val roots = HashSet<ComponentNode>()
-        val nodes = ArrayList<ComponentNode>()
+        val roots = HashSet<Node>()
+        val nodes = ArrayList<Node>()
 
         for (component in components) {
             if (component.firstPort == 0) {
-                roots.add(ComponentNode(component))
+                roots.add(Node(component))
             }
-            nodes.add(ComponentNode(component))
+            nodes.add(Node(component))
         }
 
         for (root in roots) {
@@ -119,14 +107,14 @@ object Day24 {
     private fun getLongestStrongestBridge(components: List<Component>): Int {
         var strength = 0
         var length = 0
-        val roots = HashSet<ComponentNode>()
-        val nodes = ArrayList<ComponentNode>()
+        val roots = HashSet<Node>()
+        val nodes = ArrayList<Node>()
 
         for (component in components) {
             if (component.firstPort == 0) {
-                roots.add(ComponentNode(component))
+                roots.add(Node(component))
             }
-            nodes.add(ComponentNode(component))
+            nodes.add(Node(component))
         }
 
         for (root in roots) {
@@ -141,66 +129,57 @@ object Day24 {
         return strength
     }
 
-    private fun getMaxLengthAndStrength(root: ComponentNode, nodes: List<ComponentNode>): Result {
+    private fun getMaxLengthAndStrength(root: Node, nodes: List<Node>): Result {
         var strength = root.component.firstPort + root.component.secondPort
         var length = 1
 
-        val stack = Stack<ComponentNode>()
+        val stack = Stack<Node>()
         stack.add(root)
 
         while (!stack.isEmpty()) {
             val currentNode = stack.pop()
             val stackSize = stack.size
             for (node in nodes) {
-                val nodeCopy = ComponentNode(node.component)
+                val nodeCopy = Node(node.component)
                 if (currentNode.connect(nodeCopy)) {
+                    nodeCopy.strength += currentNode.strength
+                    nodeCopy.length = currentNode.length + 1
+                    nodeCopy.parent = currentNode
                     stack.add(nodeCopy)
-                    nodeCopy.parents = LinkedHashSet(currentNode.parents)
-                    nodeCopy.parents.add(currentNode.component)
                 }
             }
-            if (stackSize == stack.size && currentNode.parents.size + 1 >= length) {
-                length = currentNode.parents.size + 1
-                strength = Math.max(strength, countStrength(currentNode))
+            if (stackSize == stack.size && currentNode.length >= length) {
+                length = currentNode.length
+                strength = Math.max(strength, currentNode.strength)
             }
         }
         return Result(length, strength)
     }
 
-    private fun getMaxStrength(root: ComponentNode, nodes: List<ComponentNode>): Int {
+    private fun getMaxStrength(root: Node, nodes: List<Node>): Int {
         var strength = 0
-        val stack = Stack<ComponentNode>()
+        val stack = Stack<Node>()
         stack.add(root)
 
         while (!stack.isEmpty()) {
             val currentNode = stack.pop()
             val stackSize = stack.size
             for (node in nodes) {
-                val nodeCopy = ComponentNode(node.component)
+                val nodeCopy = Node(node.component)
                 if (currentNode.connect(nodeCopy)) {
+                    nodeCopy.strength += currentNode.strength
+                    nodeCopy.length = currentNode.length + 1
+                    nodeCopy.parent = currentNode
                     stack.add(nodeCopy)
-                    nodeCopy.parents = LinkedHashSet(currentNode.parents)
-                    nodeCopy.parents.add(currentNode.component)
                 }
             }
             if (stack.size == stackSize) {
                 // No elements added, so we can count the strength
                 // from this node to the top
-                strength = Math.max(strength, countStrength(currentNode))
+                strength = Math.max(strength, currentNode.strength)
             }
         }
 
         return strength
     }
-
-    private fun countStrength(node: ComponentNode): Int {
-        var strength = node.component.firstPort + node.component.secondPort
-
-        for (parent in node.parents) {
-            strength += parent.firstPort + parent.secondPort
-        }
-
-        return strength
-    }
-
 }
